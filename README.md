20451c6ff170196850a41254221270adaa7945dc
James McCormac <jmccormac001@gmail.com>
2017-04-10 12:34:57 +0100

    initial commit of install script

---
+++ README.md
@@ -0,0 +1,12 @@
+# Python Setup
+
+This script installs all the necessary bits for a new python installation
+
+# Contributors
+
+James McCormac
+
+# License
+
+MIT
+
---
+++ install.sh
@@ -0,0 +1,19 @@
+# python setup script
+pip install numpy
+pip install scipy
+pip install pandas
+pip install astropy
+pip install donuts
+pip install pyqt5
+pip install matplotlib
+pip install sep
+pip install ngtsio
+pip install scikit-image
+pip install reproject
+pip install pyregion
+pip install pylint
+pip install pyephem
+pip install jupyter
+pip install jupyterthemes
+pip install ccdproc
+
